11306d3234091bd74a628b8e2f6c1f458ac0c054
jaakko <jaakko@jaakko.local>
2024-12-04 14:45:59 +0200

    day4

--- day4/day4.py
+++ day4/day4.py
@@ -1,7 +1,7 @@
 
 def p1(data):
     
-    xmas = ['X', 'M', 'A', 'S']; count = 0
+    xmas_samx = [['X', 'M', 'A', 'S'], ['S', 'A', 'M', 'X']] ; count = 0
 
     for y, row in enumerate(data):
         for x in range(len(row)):
@@ -12,7 +12,7 @@ def p1(data):
                 dl = [data[y-j][x-j] for j in range(4)] if x >= 3 else None
                 dr = [data[y-j][x+j] for j in range(4)] if x+3 < len(row) else None
 
-                count += sum([d in [xmas, xmas[::-1]] for d in [vert, dl, dr]])
+                count += sum([d in xmas_samx for d in [vert, dl, dr]])
 
         # horizontal
         count += row.count("XMAS") + row.count("SAMX")
@@ -22,7 +22,7 @@ def p1(data):
 
 def p2(data):
     
-    mas = ['M', 'A', 'S']; count = 0
+    mas_sam = [['M', 'A', 'S'], ['S', 'A', 'M']] ; count = 0
 
     for y in range(1, len(data)-1):
         for x in range(1, len(data[y])-1):
@@ -30,7 +30,7 @@ def p2(data):
             d1 = [data[y+j][x+j] for j in range(-1, 2)]
             d2 = [data[y+j][x-j] for j in range(-1, 2)]
 
-            count += (d1==mas or d1==mas[::-1]) and (d2==mas or d2==mas[::-1])
+            count += d1 in mas_sam and d2 in mas_sam
 
     return count
 
