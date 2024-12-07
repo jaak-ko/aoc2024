12a7caf7cf86d946f55c6e79c26524b3f498f258
jaakko <jaakko@jaakko.local>
2024-12-07 12:56:43 +0200

    day7

--- day7/day7.py
+++ day7/day7.py
@@ -1,9 +1,9 @@
 from itertools import product
 from operator import add, mul
 
-def valid_line(correct_res, line, p1=True):
+def valid_line(correct_res, line, p2=False):
 
-    ops = [add, mul] if p1 else [add, mul, 'or']
+    ops = [add, mul] if not p2 else [add, mul, lambda a,b: int(str(a) + str(b))]
 
     nums = list(map(int, line.strip().split(' ')))
     op_combs = list(product(ops, repeat=len(nums)-1))
@@ -11,9 +11,7 @@ def valid_line(correct_res, line, p1=True):
     for op_comb in op_combs:
         res = nums[0]
         for j, op in enumerate(op_comb):
-
-            if op == 'or': res = int(str(res) + str(nums[j+1])) ; j += 1
-            else: res = op(res, nums[j+1])
+            res = op(res, nums[j+1])
 
         if int(correct_res) == res: return True
 
@@ -29,13 +27,13 @@ def p1(data):
 def p2(data, valid_lines):
 
     invalids = list(filter(lambda x : x not in valid_lines, data))
-    lines = list(filter(lambda x : valid_line(*x, p1=False), invalids))
+    lines = list(filter(lambda x : valid_line(*x, True), invalids))
     return sum(int(i[0]) for i in lines)
 
 
 def main():
 
-    with open('day7/input.txt', 'r') as f:
+    with open('day7/example.txt', 'r') as f:
         data = [x.split(':') for x in f.read().splitlines()]
 
     p1_res, valid_lines = p1(data)
