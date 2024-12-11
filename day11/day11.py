4545b0301565c7beaeccb1585d8b82bc4cb6541e
jaakko <jaakko@jaakko.local>
2024-12-11 16:21:31 +0200

    day11

--- day11/day11.py
+++ day11/day11.py
@@ -1,14 +1,16 @@
 from functools import cache
 
-@cache 
+@cache
 def count(stone, iterations):
 
     # if you're wondering, i myself am not this smart
 
     if iterations == 0: return 1
     if stone == 0: return count(1, iterations-1)
+
     string = str(stone)
     length = len(string)
+
     if length % 2 == 0:
         return count(int(string[:length//2]), iterations - 1) + \
                count(int(string[length//2:]), iterations - 1)
@@ -20,8 +22,8 @@ def main():
     with open('day11/input.txt') as f:
         data = list(map(int, f.read().strip().split(' ')))
 
-    print(f"part 1: {sum(count(stone, 25) for stone in data)}")
-    print(f"part 2: {sum(count(stone, 75) for stone in data)}")
+    for i, part in zip((25, 75), ("part 1", "part 2")):
+        print(f"{part}: {sum(count(stone, i) for stone in data)}")
 
 if __name__ == '__main__':
     main()
